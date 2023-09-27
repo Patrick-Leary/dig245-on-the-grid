c998ccd822c0c38e6cb26ab1b831d9fe7d7e481b
Patrick Leary <paleary@davidson.edu>
2023-09-27 18:24:29 -0400

    Changed some bootstrap layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,5 @@
 
 /* javascript */
+
+console.log("Figma Link:")
+console.log("https://www.figma.com/file/LThUlZXxFSfGzVEwvTYWBq/On-The-Grid?type=design&node-id=3-2&mode=design&t=n3aQux3GtGaYRNQj-0")
